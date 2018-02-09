53f92df96838627767129c441fe4b40c88db0bf9
yossish <yossi.shmulevitch@gmail.com>
2018-02-09 23:59:05 +0200

    selection on card

--- src/pages/cards/cards.ts
+++ src/pages/cards/cards.ts
@@ -11,12 +11,14 @@ export class CardsPage {
 	
   //users: any;
   benefits: any;
+  selectedBenefits: any;
   cards: any;
   token: any;
 
   constructor(public navCtrl: NavController,public navParams: NavParams, public restProvider: RestProvider) {
   	
   	this.token = navParams.get ("token");
+    this.selectedBenefits  = [];
   	this.getBenefits();
   	
 
@@ -26,10 +28,29 @@ export class CardsPage {
   	/* this.cards=[{"_id":"5a6dd24fafb6271d40ead72c","implementMethod":"אשראי","provider":"לאומי קארד","name":"executive","image":"images/leumi-card.png","bg":"images/executive.png","__v":0},{"_id":"5a6dd24fafb6271d40ead72b","implementMethod":"אשראי","provider":"לאומי קארד","name":"לאומי קארד","image":"images/leumi-card.png","bg":"images/leumi_card.png","__v":0},{"_id":"5a6dd24fafb6271d40ead72d","implementMethod":"אשראי","provider":"Diners","name":"You","image":"images/dinners_you.jpg","bg":"images/you-bg.png","__v":0},{"_id":"5a6dd24fafb6271d40ead72e","implementMethod":"מועדון","provider":"You","name":"You","image":"images/you_club.png","__v":0},{"_id":"5a6dd24fafb6271d40ead72f","implementMethod":"מועדון","provider":"","name":"Dream Card","image":"images/dream-card.png","bg":"images/dream-card.png","__v":0},{"_id":"5a6dd24fafb6271d40ead730","implementMethod":"אשראי","provider":"","name":"מולטי עזריאלי","image":"images/leumi-card.png","bg":"images/multi_azrieli.png","__v":0},{"_id":"5a6dd24fafb6271d40ead731","implementMethod":"אשראי","provider":"","name":"חבר","image":"images/hever_logo.png","bg":"images/hever.png","__v":0},{"_id":"5a6dd24fafb6271d40ead732","implementMethod":"אשראי","provider":"ויזה","name":"כאל","image":"images/cal_logo.png","bg":"images/cal_logo.png","__v":0},{"_id":"5a6dd24fafb6271d40ead734","implementMethod":"מועדון","provider":"","name":"iStudent","image":"images/istudent.png","bg":"images/istudent.png","__v":0},{"_id":"5a6dd24fafb6271d40ead736","implementMethod":"אשראי","provider":"visa","name":"מפתח דיסקונט","image":"images/discont.png","bg":"images/discont.png","__v":0},{"_id":"5a6dd24fafb6271d40ead735","implementMethod":"מועדון","provider":"","name":"עדיף","image":"images/adif.png","bg":"images/adif.png","__v":0},{"_id":"5a6dd24fafb6271d40ead733","implementMethod":"מועדון","provider":"","name":"יותר","image":"images/Yoter.png","bg":"images/Yoter.png","__v":0},{"_id":"5a6dd91b734d1d63031971f8","implementMethod":"מועדון","provider":"","name":"הוט","image":"images/hot_logo.png","bg":"images/hot.png"},{"_id":"5a6ddbd2734d1d63031972e5","implementMethod":"אשראי","provider":"Diners","name":"Fly Card","image":"images/fly_card_logo.png","bg":"images/fly_card.png"}]; */
   }
 
-  chooseCard() {
-  	alert ("chooseCard");
+  chooseCard(index) {
+  	let card = document.getElementById('card'+index);
+    let cardTitle = document.getElementById('cardTitle'+index);
+  	//alert ("chooseCard: " + index + " card: " + card.className);
+  	if( card.className.indexOf('card-z-depth-5') >= 0){
+      // selected
+      var selectedIndex = this.selectedBenefits.findIndex(function(benefit) {
+        return benefit.club.name == cardTitle.innerText;
+      });
+      this.selectedBenefits.splice (selectedIndex,1);
+      card.className = card.className.replace(" card-z-depth-5", "");
 
+    }else{
+      alert ("chooseCard: " + index + " card: " +this.benefits [index].club.name);
+      this.selectedBenefits.push (this.benefits [index]);
+      card.className+=' card-z-depth-5';
+    }
+    //alert ("chooseCard: " + index + " card: " + card.className);
+  // Found world
   }
+  	
+
+  
 
   getBenefits() {
     this.restProvider.getBenefits(this.token)
